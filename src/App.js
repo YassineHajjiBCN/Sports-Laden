5bfdb6c2b4f637026090bdc4d9b95be6797af443
Yassine <yassinehajji93its@gmail.com>
2020-10-15 01:37:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
+import data from './data';
 import './App.css';
+import { BrowserRouter, Router } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className="grid-container">
+    <header className="header">
+     <div className="brand">
+       <a href="index.html" >Sport Laden</a>
+     </div>
+     <div className="header-links">
+       <a className="cart" href="Cart.html">Cart</a>
+       <a href="Signin.html">Sign In</a>
+     </div>
+    </header>
+    <main className="main">
+      <div className="content">
+        <Router path="/products/:id" component={ProductScreen} /> 
+        <Router path="/" exact={true} component={HomeScreen} />
+        <ul className="products">
+          
+          { 
+            data.products.map(product =>
+
+        <li >
+          <div className="product">
+            <img className="product-image" src={product.image} alt="product-image" />
+            <div className="product-name">
+              <a href="prod.html">{product.name}</a>
+            </div>
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">CHF{product.price}</div>
+            <div className="product-rating">4 {product.rating} stars {product.numReviews}</div>
+
+             
+          </div>
+        </li>
+           )}
+      </ul>
+      </div>
+      
+    </main>
+    <footer className="footer">
+      All right reserved - Sports Laden
+    </footer>
+  </div>
+  </BrowserRouter>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default {
+ products: [{
+   name: 'Jacket',
+   category: 'jacket',
+   image: '/img/p1.png',
+   price: 49,
+   brand: 'New Balance',
+   rating: 4.5,
+   numReviwes: 1130
+ },
+ {
+  name: 'Shirt',
+  category: 'shirt',
+  image: '/img/p1.png',
+  price: 49,
+  brand: 'Adidas',
+  rating: 3.8,
+  numReviwes: 2230
+},
+{
+  name: 'Jordan',
+  category: 'shoes',
+  image: '/img/p1.png',
+  price: 459,
+  brand: 'Nike',
+  rating: 4.9,
+  numReviwes: 1130
+}
+]
+}
